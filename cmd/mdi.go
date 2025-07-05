97c8370a825e2db48e5f893a2a6a0ed7a4a5c36e
TS22082 <ts22082@gmail.com>
2025-07-04 19:55:53 -0700

    update dexcriptions

--- cmd/mdi.go
+++ cmd/mdi.go
@@ -1,14 +1,15 @@
 package cmd
 
 import (
+	"github.com/TS22082/ts_cli_tool/descriptions"
 	"github.com/TS22082/ts_cli_tool/handlers"
 	"github.com/spf13/cobra"
 )
 
 var mdiCmd = &cobra.Command{
 	Use:   "mdi",
-	Short: "mdi",
-	Long:  `mdi`,
+	Short: descriptions.MdiShort,
+	Long:  descriptions.MdiLong,
 	Run:   handlers.MDIHandler,
 }
 
---
+++ descriptions/mdi.go
@@ -0,0 +1,10 @@
+package descriptions
+
+const MdiShort = "Mongo Db Interface: Uses AI to generate MongoDB queries"
+
+const MdiLong = `Uses AI to generate MongoDB queries. Will prompt you for a DB name and a prompt
+
+Example:
+
+es mdi
+`

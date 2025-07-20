2553a888e879d06749d6612f3d55eb35956c46a6
TS22082 <ts22082@gmail.com>
2025-07-20 15:39:04 -0700

    fix sassy script

--- handlers/sassy.go
+++ handlers/sassy.go
@@ -2,32 +2,27 @@ package handlers
 
 import (
 	"fmt"
-	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
 
 func SassyHandler(cmd *cobra.Command, args []string) {
 
-	dir, err := os.Getwd()
-
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-
-	srcDir := dir + "/static/src/css/style.scss"
-	distDir := dir + "/static/dist/css/style.css"
+	srcDir := "./static/src/css/styles.scss"
+	distDir := "./static/dist/css/style.css"
 
 	sassCompileCmd := exec.Command("sass", srcDir, distDir)
 
-	_, err = sassCompileCmd.Output()
+	_, err := sassCompileCmd.Output()
 
 	if err != nil {
 		fmt.Println("Error:", err)
 		fmt.Println("Src Dir:", srcDir)
 		fmt.Println("Dist Dir:", distDir)
+		fmt.Println(strings.Join(sassCompileCmd.Args, " "))
+
 		return
 	}
 
